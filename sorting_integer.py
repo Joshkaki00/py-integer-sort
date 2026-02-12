abaf35574a95f1237ee7335c6a334d331a19fea3
Joshkaki00 <josh.kakinuki@students.dominican.edu>
2026-02-12 12:14:51 -0800

    Implement output list population in counting_sort function

--- sorting_integer.py
+++ sorting_integer.py
@@ -20,6 +20,15 @@ def counting_sort(numbers):
     # Step 3: Loop over given numbers and increment each number's count
     for num in numbers:
         counts[num - min_val] += 1
+    
+    # Step 4: Loop over counts and append that many numbers into output list
+    index = 0
+    for value in range(min_val, max_val + 1):
+        count = counts[value - min_val]
+        for _ in range(count):
+            numbers[index] = value
+            index += 1
+
 
 def bucket_sort(numbers, num_buckets=10):
     """Sort given numbers by distributing into buckets representing subranges,
